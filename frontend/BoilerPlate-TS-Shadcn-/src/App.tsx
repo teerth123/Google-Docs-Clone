483aad5c8fd08952dc777c4afc6122440ab99f54
DexT_17 Teerth <teerth.kulkarni@walchandsangli.ac.in>
2025-06-28 10:07:58 +0530

    girst commit

---
+++ frontend/BoilerPlate-TS-Shadcn-/src/App.tsx
@@ -0,0 +1,14 @@
+// import { Button } from "@/components/ui/button"
+import { SimpleEditor } from '@/components/tiptap-templates/simple/simple-editor'
+
+function App() {
+  return (
+    <>
+      <div className="h-screen min-w-screen bg-[#F9FBFD] ">
+        <SimpleEditor />
+      </div>
+    </>
+  )
+}
+
+export default App  
